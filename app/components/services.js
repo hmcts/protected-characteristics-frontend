6f3f8b0213c0f97c386cdb2228029a367fde9b1a
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-05 11:32:18 +0000

    Remove authorse from Services

--- app/components/services.js
+++ app/components/services.js
@@ -3,11 +3,7 @@
 const utils = require('app/components/api-utils');
 const config = require('app/config');
 const submitData = require('app/components/submit-data');
-const otp = require('otp');
 const ORCHESTRATION_SERVICE_URL = config.services.orchestration.url;
-const SERVICE_AUTHORISATION_URL = `${config.services.idam.s2s_url}/lease`;
-const serviceName = config.services.idam.service_name;
-const secret = config.services.idam.service_key;
 const FEATURE_TOGGLE_URL = config.featureToggles.url;
 const logger = require('app/components/logger');
 const logInfo = (message, applicationId = 'Init') => logger(applicationId).info(message);
@@ -35,21 +31,7 @@ const sendToOrchestrationService = (data, ctx) => {
     return utils.fetchJson(`${ORCHESTRATION_SERVICE_URL}/forms/${data.applicationId}/submissions`, fetchOptions);
 };
 
-const authorise = (applicationId) => {
-    logInfo('authorise for serviceAuthToken called', applicationId);
-    const headers = {
-        'Content-Type': 'application/json'
-    };
-    const params = {
-        microservice: serviceName,
-        oneTimePassword: otp({secret: secret}).totp()
-    };
-    const fetchOptions = utils.fetchOptions(params, 'POST', headers);
-    return utils.fetchText(SERVICE_AUTHORISATION_URL, fetchOptions);
-};
-
 module.exports = {
     sendToOrchestrationService,
-    authorise,
     featureToggle
 };
