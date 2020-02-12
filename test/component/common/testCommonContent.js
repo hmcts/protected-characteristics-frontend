f68d3b70c86d31394922b28be3a986b9e35dddbd
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-12 19:49:12 +0000

    Fix Common Content component test

--- test/component/common/testCommonContent.js
+++ test/component/common/testCommonContent.js
@@ -4,45 +4,20 @@ const TestWrapper = require('test/util/TestWrapper');
 const commonContent = require('app/resources/en/translation/common');
 
 class TestCommonContent {
-    static runTest(page, beforeEach, afterEach) {
+    static runTest(page) {
         describe('Test the help content', () => {
             const testWrapper = new TestWrapper(page);
 
             it('test help block content is loaded on page', (done) => {
-                if (typeof beforeEach === 'function') {
-                    beforeEach();
-                }
-
-                const sessionData = {
-                    applicant: {
-                        firstName: 'FirstName'
-                    }
+                const playbackData = {
+                    whyAskingParagraph: commonContent.whyAskingParagraph,
+                    whyAsking: commonContent.whyAsking
                 };
 
-                testWrapper.agent.post('/prepare-session/form')
-                    .send(sessionData)
-                    .then(() => {
-                        const playbackData = {
-                            helpTitle: commonContent.helpTitle,
-                            helpHeading1: commonContent.helpHeading1,
-                            helpHeading2: commonContent.helpHeading2,
-                            helpHeading3: commonContent.helpHeading3,
-                            helpTelephoneNumber: commonContent.helpTelephoneNumber,
-                            helpTelephoneOpeningHours: commonContent.helpTelephoneOpeningHours,
-                            helpEmailLabel: commonContent.helpEmailLabel.replace(/{contactEmailAddress}/g, commonContent.helpEmail)
-                        };
-
-                        testWrapper.testDataPlayback(done, playbackData);
-                    })
-                    .catch(err => {
-                        done(err);
-                    });
+                testWrapper.testDataPlayback(done, playbackData);
             });
 
             testWrapper.destroy();
-            if (typeof afterEach === 'function') {
-                afterEach();
-            }
         });
     }
 }
