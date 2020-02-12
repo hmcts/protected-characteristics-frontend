939b4098c77057f106a01f953ef0baccb7d63982
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-12 19:48:59 +0000

    Add Other Details options to Ethinc questions

--- app/assets/javascripts/mutually-exclusive-checkboxes.js
+++ app/assets/javascripts/mutually-exclusive-checkboxes.js
@@ -1,23 +1,28 @@
 (function() {
     'use strict';
 
+    const toggleElements = (elementsList1, elementsList2) => {
+        for (const selectedElement of elementsList1) {
+            selectedElement.addEventListener('click', () => {
+                for (const elementToDeselect of elementsList2) {
+                    const controls = elementToDeselect.getAttribute('aria-controls');
+                    if (controls) {
+                        console.log(elementToDeselect);
+                        console.log('Controls: ', controls);
+                        const controlledElement = document.querySelector('#' + controls);
+                        controlledElement.classList.add('govuk-checkboxes__conditional--hidden');
+                    }
+
+                    elementToDeselect.checked = false;
+                    elementToDeselect.setAttribute('aria-expanded', 'false');
+                }
+            }, false);
+        }
+    };
+
     const $selectCheckboxes = document.querySelectorAll('[data-mutually-exclusive="js-select-choice"]');
     const $deselectCheckboxes = document.querySelectorAll('[data-mutually-exclusive="js-deselect-choice"]');
 
-    for (const selectedElement of $selectCheckboxes) {
-        selectedElement.addEventListener('click', () => {
-            for (const elementToDeselect of $deselectCheckboxes) {
-                elementToDeselect.checked = false;
-            }
-        }, false);
-    }
-
-    for (const selectedElement of $deselectCheckboxes) {
-        selectedElement.addEventListener('click', () => {
-            for (const elementToDeselect of $selectCheckboxes) {
-                elementToDeselect.checked = false;
-            }
-        }, false);
-    }
+    toggleElements($selectCheckboxes, $deselectCheckboxes);
+    toggleElements($deselectCheckboxes, $selectCheckboxes);
 }).call(this);
-
