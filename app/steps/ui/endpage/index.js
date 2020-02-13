1f2dc5d5d7809318c18f5194b90cc775730ef2df
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-13 16:40:25 +0000

    Hide return button if return url not available

--- app/steps/ui/endpage/index.js
+++ app/steps/ui/endpage/index.js
@@ -7,6 +7,19 @@ class EndPage extends Step {
     static getUrl () {
         return '/end-page';
     }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        ctx.returnUrl = req.session.returnUrl;
+
+        return ctx;
+    }
+
+    action(ctx, formdata) {
+        super.action(ctx, formdata);
+        delete ctx.returnUrl;
+        return [ctx, formdata];
+    }
 }
 
 module.exports = EndPage;
