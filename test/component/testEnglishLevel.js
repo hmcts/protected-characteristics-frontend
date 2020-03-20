2bf5f5e6baee0a8d15cbfd5383eb09ded7f65f79
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-03-20 10:12:02 +0000

    Add additional english level component tests

--- test/component/testEnglishLevel.js
+++ test/component/testEnglishLevel.js
@@ -25,9 +25,41 @@ describe('ApplicantEnglishLevel', () => {
             testWrapper.testContent(done);
         });
 
-        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex}`, (done) => {
+        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex} - english level 1`, (done) => {
             const data = {
                 english_language_level: 1
+            };
+
+            testWrapper.testRedirect(done, data, expectedNextUrlForApplicantSex);
+        });
+
+        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex} - english level 2`, (done) => {
+            const data = {
+                english_language_level: 2
+            };
+
+            testWrapper.testRedirect(done, data, expectedNextUrlForApplicantSex);
+        });
+
+        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex} - english level 3`, (done) => {
+            const data = {
+                english_language_level: 3
+            };
+
+            testWrapper.testRedirect(done, data, expectedNextUrlForApplicantSex);
+        });
+
+        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex} - english level 4`, (done) => {
+            const data = {
+                english_language_level: 4
+            };
+
+            testWrapper.testRedirect(done, data, expectedNextUrlForApplicantSex);
+        });
+
+        it(`test it redirects to applicant sex page: ${expectedNextUrlForApplicantSex} - english level 0`, (done) => {
+            const data = {
+                english_language_level: 0
             };
 
             testWrapper.testRedirect(done, data, expectedNextUrlForApplicantSex);
