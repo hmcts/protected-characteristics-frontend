fbd7f5f3369d43ad43734e684c11424b77c42bfd
riyan <venkatd648@gmail.com>
2020-03-23 18:01:08 +0000

    Conflicts resolved

--- test/end-to-end/pages/startpage/startpage.js
+++ test/end-to-end/pages/startpage/startpage.js
@@ -5,10 +5,8 @@ const serviceEndpointUrl = require('test/end-to-end/utils').serviceEndpointUrl;
 
 module.exports = function () {
     const I = this;
-    console.log('this is the url '+ serviceEndpointUrl({returnUrl: 'dummy-return-url'}));
     I.amOnPage(serviceEndpointUrl({returnUrl: 'dummy-return-url'}));
     I.seeCurrentUrlEquals(pageUnderTest.getUrl());
-    console.log('this is the url '+ pageUnderTest.getUrl())
     I.see('Continue to the question');
     I.see('I don\'t want to answer these questions');
     I.click('Continue to the question');
