23140cfb7c03cbda6aa69a10f72acd0ad6ef08c2
riyan <venkatd648@gmail.com>
2020-03-24 12:10:46 +0000

    updated saucelabs config

--- test/end-to-end/saucelabs.conf.js
+++ test/end-to-end/saucelabs.conf.js
@@ -1,58 +1,90 @@
+/* eslint-disable no-console */
+
 const supportedBrowsers = require('../crossbrowser/supportedBrowsers.js');
+const CONF = require('config');
 
-const browser = process.env.SAUCELABS_BROWSER || 'chrome';
-const tunnelName = process.env.TUNNEL_IDENTIFIER || '';
-
-const setupConfig = {
-    'tests': './paths/*.js',
-    'output': './output',
-    'timeout': 20000,
-    'helpers': {
-        WebDriverIO: {
-            url: process.env.TEST_E2E_FRONTEND_URL || 'https://localhost:3000',
-            browser: supportedBrowsers[browser].browserName,
-            waitforTimeout: 60000,
-            cssSelectorsEnabled: 'true',
-            windowSize: '1600x900',
-            timeouts: {
-                script: 60000,
-                'page load': 60000,
-                implicit: 20000
-            },
-            'host': 'ondemand.saucelabs.com',
-            'port': 80,
-            'user': process.env.SAUCE_USERNAME,
-            'key': process.env.SAUCE_ACCESS_KEY,
-            desiredCapabilities: getDesiredCapabilities()
-        },
-
-        'JSWait': {
-            'require': './helpers/JSWait.js'
-        },
-
-        'SauceLabsReportingHelper': {
-            'require': './helpers/SauceLabsReportingHelper.js'
+const waitForTimeout = parseInt(CONF.saucelabs.waitForTimeoutValue);
+const smartWait = parseInt(CONF.saucelabs.smartWait);
+const browser = process.env.SAUCE_BROWSER || CONF.saucelabs.browser;
+const tunnelName = process.env.SAUCE_TUNNEL_IDENTIFIER || CONF.saucelabs.tunnelId;
+const getBrowserConfig = (browserGroup) => {
+    const browserConfig = [];
+    for (const candidateBrowser in supportedBrowsers[browserGroup]) {
+        if (candidateBrowser) {
+            const desiredCapability = supportedBrowsers[browserGroup][candidateBrowser];
+            desiredCapability.tunnelIdentifier = tunnelName;
+            desiredCapability.tags = ['pcq'];
+            browserConfig.push({
+                browser: desiredCapability.browserName,
+                desiredCapabilities: desiredCapability
+            });
+        } else {
+            console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
         }
-    },
-    'include': {
-        'I': './pages/steps.js'
-    },
-    'mocha': {
-        'reporterOptions': {
-            'reportDir': process.env.E2E_CROSSBROWSER_OUTPUT_DIR || './output',
-            'reportName': browser + '_report',
-            'reportTitle': 'Crossbrowser results for: ' + browser.toUpperCase(),
-            'inlineAssets': true
-        }
-    },
-    'name': 'frontEnd Tests'
+    }
+    return browserConfig;
 };
 
-function getDesiredCapabilities() {
-    const desiredCapability = supportedBrowsers[browser];
-    desiredCapability.tunnelIdentifier = tunnelName;
-    desiredCapability.tags = ['pcq'];
-    return desiredCapability;
-}
-
+const setupConfig = {
+    tests: './step_definitions/probatepcqjourney.js',
+    output: process.cwd() + '/functional-output',
+    helpers: {
+        WebDriverIO: {
+            url: process.env.E2E_FRONTEND_URL || CONF.e2e.frontendUrl,
+            browser,
+            waitForTimeout,
+            smartWait,
+            cssSelectorsEnabled: 'true',
+            host: 'ondemand.eu-central-1.saucelabs.com',
+            port: 80,
+            region: 'eu',
+            user: process.env.SAUCE_USERNAME || CONF.saucelabs.username,
+            key: process.env.SAUCE_ACCESS_KEY || CONF.saucelabs.key,
+            desiredCapabilities: {}
+        },
+        SauceLabsReportingHelper: {require: './helpers/SauceLabsReportingHelper.js'},
+        JSWait: {require: './helpers/JSWait.js'},
+        ElementExist: {require: './helpers/ElementExist.js'},
+        IdamHelper: {require: './helpers/idamHelper.js'},
+        SessionHelper: {require: './helpers/SessionHelper.js'}
+    },
+    include: {
+        I: 'test/end-to-end/pages/steps.js'
+    },
+    mocha: {
+        reporterOptions: {
+            'codeceptjs-cli-reporter': {
+                stdout: '-',
+                options: {steps: true}
+            },
+            'mocha-junit-reporter': {
+                stdout: '-',
+                options: {mochaFile: `${CONF.e2e.outputDir}/result.xml`}
+            },
+            mochawesome: {
+                stdout: './functional-output/console.log',
+                options: {
+                    reportDir: CONF.e2e.outputDirectory,
+                    reportName: 'index',
+                    inlineAssets: true
+                }
+            }
+        }
+    },
+    multiple: {
+        microsoftIE11: {
+            browsers: getBrowserConfig('microsoftIE11')
+        },
+        microsoftEdge: {
+            browsers: getBrowserConfig('microsoftEdge')
+        },
+        chrome: {
+            browsers: getBrowserConfig('chrome')
+        },
+        firefox: {
+            browsers: getBrowserConfig('firefox')
+        }
+    },
+    name: 'Pcq Frontend Tests'
+};
 exports.config = setupConfig;
