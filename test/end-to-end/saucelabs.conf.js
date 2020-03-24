d5833d11bced796edeb668ac0e9de948e1c5a5cc
riyan <venkatd648@gmail.com>
2020-03-24 19:29:50 +0000

    updated sauce labs config

--- test/end-to-end/saucelabs.conf.js
+++ test/end-to-end/saucelabs.conf.js
@@ -1,48 +1,22 @@
-/* eslint-disable no-console */
-const supportedBrowsers = require('../crossbrowser/supportedBrowsers.js');
-
+const supportedBrowsers = require('./src/test/js/config/supportedBrowsers.js');
+const browser = process.env.SAUCELABS_BROWSER || 'chrome';
 const tunnelName = process.env.TUNNEL_IDENTIFIER || 'reformtunnel';
-
-const getBrowserConfig = (browserGroup) => {
-    const browserConfig = [];
-    for (const candidateBrowser in supportedBrowsers[browserGroup]) {
-        if (candidateBrowser) {
-            const desiredCapability = supportedBrowsers[browserGroup][candidateBrowser];
-            desiredCapability.tunnelIdentifier = tunnelName;
-            desiredCapability.acceptSslCerts = 'true';
-            desiredCapability.acceptInsecureCerts = 'true';
-            desiredCapability.tags = ['pcq'];
-            browserConfig.push({
-                browser: desiredCapability.browserName,
-                desiredCapabilities: desiredCapability
-            });
-        } else {
-            console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
-        }
-    }
-    return browserConfig;
-};
-
 const setupConfig = {
-    output: process.cwd() + '/functional-output',
+    output: './functional-output',
+    timeout: 60000,
     helpers: {
-        WebDriver: {
-            url: process.env.E2E_FRONTEND_URL ||'https://pcq-frontend-staging.service.core-compute-aat.internal',
-            browser: 'chrome',
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors', '--allow-running-insecure-content'],
-            waitforTimeout: 60000,
-            ignoreHTTPSErrors: true,
-            cssSelectorsEnabled: true,
-            windowSize: '1600x900',
+        WebDriverIO: {
+            url: process.env.TEST_URL,
+            browser: supportedBrowsers[browser].browserName,
+            cssSelectorsEnabled: 'true',
             host: 'ondemand.eu-central-1.saucelabs.com',
             port: 80,
             region: 'eu',
-            user: process.env.SAUCE_USERNAME || 'username',
-            key: process.env.SAUCE_ACCESS_KEY || 'privatekey',
-            desiredCapabilities: {}
-        },
-        SauceLabsReportingHelper: {require: './helpers/SauceLabsReportingHelper.js'},
-        JSWait: {require: './helpers/JSWait.js'}
+            user: process.env.SAUCE_USERNAME,
+            key: process.env.SAUCE_ACCESS_KEY,
+            desiredCapabilities: getDesiredCapabilities(),
+            waitforTimeout: 60000
+        }
     },
     gherkin: {
         features: 'features/probate.feature',
@@ -53,38 +27,17 @@ const setupConfig = {
     },
     mocha: {
         reporterOptions: {
-            'codeceptjs-cli-reporter': {
-                stdout: '-',
-                options: {steps: true}
-            },
-            'mocha-junit-reporter': {
-                stdout: '-',
-                options: {mochaFile: '/functional-output/result.xml'}
-            },
-            mochawesome: {
-                stdout: './functional-output/console.log',
-                options: {
-                    reportDir: './functional-output',
-                    reportName: 'index',
-                    inlineAssets: true
-                }
-            }
+            reportDir: './functional-output',
+            reportName: browser + '_report',
+            reportTitle: 'Crossbrowser results for: ' + browser.toUpperCase(),
+            inlineAssets: true
         }
     },
-    multiple: {
-        microsoftIE11: {
-            browsers: getBrowserConfig('microsoftIE11')
-        },
-        microsoftEdge: {
-            browsers: getBrowserConfig('microsoftEdge')
-        },
-        chrome: {
-            browsers: getBrowserConfig('chrome')
-        },
-        firefox: {
-            browsers: getBrowserConfig('firefox')
-        }
-    },
-    name: 'Pcq Frontend Tests'
 };
+function getDesiredCapabilities() {
+    const desiredCapability = supportedBrowsers[browser];
+    desiredCapability.tunnelIdentifier = tunnelName;
+    desiredCapability.acceptSslCerts = true;
+    return desiredCapability;
+}
 exports.config = setupConfig;
