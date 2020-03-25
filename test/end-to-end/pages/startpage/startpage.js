8eccb7235a3ddfeae0d7f9c8ee0c17ba66acc2d4
riyan <venkatd648@gmail.com>
2020-03-25 19:18:34 +0000

    updated sauce config

--- test/end-to-end/pages/startpage/startpage.js
+++ test/end-to-end/pages/startpage/startpage.js
@@ -1,12 +1,12 @@
 'use strict';
 
-//const pageUnderTest = require('app/steps/ui/startpage/index');
+const pageUnderTest = require('app/steps/ui/startpage/index');
 const serviceEndpointUrl = require('test/end-to-end/utils').serviceEndpointUrl;
 
 module.exports = function () {
     const I = this;
     I.amOnPage(serviceEndpointUrl({returnUrl: 'dummy-return-url'}));
-    // I.seeCurrentUrlEquals(pageUnderTest.getUrl());
+    I.seeCurrentUrlEquals(pageUnderTest.getUrl());
     I.see('Continue to the question');
     I.see('I don\'t want to answer these questions');
     I.click('Continue to the question');
