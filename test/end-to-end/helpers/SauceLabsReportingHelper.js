d24715f6cce0c51c450075c1c9b7ca826b53a47c
riyan <venkatd648@gmail.com>
2020-03-31 22:16:02 +0100

    updated sauceLabs Reporting

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -1,12 +1,10 @@
 const event = require('codeceptjs').event;
 const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
-const sauceUsername = 'salivendra';
-const sauceKey = 'd8c2d5fc-1807-434d-bdbd-133e326416ec';
 
 function updateSauceLabsResult(result, sessionId) {
-    console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=div-petitioner-frontend'); /* eslint-disable-line no-console, prefer-template */
-    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + sauceUsername + ':' + sauceKey + ' https://eu-central-1.saucelabs.com/rest/v1/' + sauceUsername + '/jobs/' + sessionId;
+    console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=pcq-frontend');
+    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + 'https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
 module.exports = function() {
