eeed33600d72804fedfe0ec3309a893c270be9b7
riyan <venkatd648@gmail.com>
2020-03-31 18:20:35 +0100

    updated sample test script

--- test/end-to-end/pages/startpage/startpage.js
+++ test/end-to-end/pages/startpage/startpage.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const pageUnderTest = require('app/steps/ui/startpage/index');
+//const pageUnderTest = require('app/steps/ui/startpage/index');
 module.exports = function () {
     const I = this;
-    I.amOnPage(pageUnderTest.getUrl());
-    I.see('Continue to the question');
-    I.click('Continue to the question');
+    I.amOnPage('/webhp');
+    I.fillField('q', 'wikipedia');
+    I.pressKey('Enter');
+    I.see('Wikipedia');
+    console.log('Passed!...');
 };
