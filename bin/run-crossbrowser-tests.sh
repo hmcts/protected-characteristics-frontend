32e394aa3e4857320292048f6e4c2444974e772f
riyan <venkatd648@gmail.com>
2020-04-02 18:55:30 +0100

    updated sauce lab conif

---
+++ bin/run-crossbrowser-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+# Setup required for Saucelabs environment variables. TEST_URL should be set by CNP
+export E2E_FRONTEND_URL=${TEST_URL}
+export IGNORE_SESSION_VALIDATION=true
+
+EXIT_STATUS=0
+BROWSER_GROUP=microsoftIE11 yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+BROWSER_GROUP=microsoftEdge yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+BROWSER_GROUP=chrome yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+BROWSER_GROUP=firefox yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+echo EXIT_STATUS: $EXIT_STATUS
+exit $EXIT_STATUS
