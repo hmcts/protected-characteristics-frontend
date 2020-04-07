bfc5a424498d8bd2af23f359d7ed791ab84ef6e7
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-04-07 10:31:59 +0100

    Add logging

--- app/components/auth.js
+++ app/components/auth.js
@@ -2,9 +2,11 @@
 
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const logger = require('app/components/logger')('Init');
 
 const createToken = (req, partyId, payload = {}) => {
     const conf = config.auth.jwt;
+    logger.info(`Using ${conf.secret === 'JwtSecretKey' ? 'local' : 'Azure KV'} secret`);
 
     payload.authorities = [];
     const token = jwt.sign(payload, conf.secret, {subject: partyId, expiresIn: conf.ttl});
