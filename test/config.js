b41f465469f828024b0ae2e09d1edfee6b7df56a
riyan <venkatd648@gmail.com>
2020-04-09 22:24:29 +0100

    changed the browser value

--- test/config.js
+++ test/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    TestFrontendUrl: process.env.TEST_URL || 'http://localhost:4000',
     TestGetUserUrl: 'http://pcq-backend-aat.service.core-compute-aat.internal/pcq/backend/getAnswer',
     TestProxy: process.env.TEST_PROXY || 'http://proxyout.reform.hmcts.net:8080',
     TestUseProxy: process.env.TEST_USE_PROXY || 'true',
