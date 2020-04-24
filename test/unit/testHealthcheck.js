9928e10a341e47ecbe3ec9a41897ef5331cbc8af
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-04-24 12:28:13 +0100

    Add backend down test

--- test/unit/testHealthcheck.js
+++ test/unit/testHealthcheck.js
@@ -31,5 +31,30 @@ describe('Healthcheck', () => {
                     done();
                 });
         });
+
+        it('should return the correct params on PCQ Backend DOWN', (done) => {
+            nock(config.services.pcqBackend.url)
+                .get('/health')
+                .reply(
+                    500,
+                    {'status': 'DOWN'}
+                );
+
+            const server = app.init();
+            const agent = request.agent(server.app);
+            agent.get('/health')
+                .expect(500)
+                .end((err, res) => {
+                    server.http.close();
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(res.body);
+                    expect(res.body).to.have.property('status').and.equal('DOWN');
+                    expect(res.body).to.have.property('pcq-backend');
+                    expect(res.body).to.have.property('buildInfo');
+                    done();
+                });
+        });
     });
 });
