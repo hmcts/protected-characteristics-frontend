fe540be69b056119c2935e67098fbcde0e5cf0ac
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-05-06 17:00:02 +0100

    Fix back button not working on safari

--- app/assets/javascripts/back-button.js
+++ app/assets/javascripts/back-button.js
@@ -2,7 +2,10 @@
     'use strict';
 
     const backButton = document.querySelector('#back-button');
-    const back = function () { history.go(-1) };
+    const back = function (e) {
+        e.preventDefault();
+        window.history.back();
+    };
 
     backButton.addEventListener('click', back);
     backButton.addEventListener('touchstart', back);
