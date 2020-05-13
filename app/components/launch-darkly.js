62af8c095fdabe6332411caf39ada3f1addfffb3
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-05-13 17:03:12 +0100

    Add close functionality & unit tests for LD

--- app/components/launch-darkly.js
+++ app/components/launch-darkly.js
@@ -4,17 +4,18 @@ const config = require('config');
 const launchDarkly = require('launchdarkly-node-server-sdk');
 
 class LaunchDarkly {
-
-    constructor(options, ftValue = false) {
+    constructor(options, ftValue) {
         this.client = launchDarkly.init(config.featureToggles.launchDarklyKey, options);
         this.ftValue = ftValue;
     }
 
+    close() {
+        this.client.close();
+    }
 }
 
 class Singleton {
-
-    constructor(options, ftValue = false) {
+    constructor(options = {}, ftValue = false) {
         if (!Singleton.instance) {
             Singleton.instance = new LaunchDarkly(options, ftValue);
         }
@@ -24,6 +25,10 @@ class Singleton {
         return Singleton.instance;
     }
 
+    close() {
+        Singleton.instance.close();
+        Singleton.instance = null;
+    }
 }
 
 module.exports = Singleton;
---
+++ test/unit/components/testLaunchDarkly.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const expect = require('chai').expect;
+const LaunchDarkly = require('app/components/launch-darkly');
+
+describe('LaunchDarkly', () => {
+    afterEach(() => {
+        new LaunchDarkly().close();
+    });
+
+    it('should create a new launchdarkly instance', (done) => {
+        const ld = new LaunchDarkly({offline: true}, true).getInstance();
+
+        // eslint-disable-next-line no-unused-expressions
+        expect(ld.client).to.not.be.undefined;
+        expect(ld.ftValue).to.equal(true);
+        done();
+    });
+
+    it('should successfully close the LD connection and clear the instance', (done) => {
+        const ld = new LaunchDarkly({offline: true});
+        let instance = ld.getInstance();
+
+        // eslint-disable-next-line no-unused-expressions
+        expect(instance).to.not.be.undefined;
+
+        ld.close();
+        instance = ld.getInstance();
+
+        expect(instance).to.equal(null);
+        done();
+    });
+});
