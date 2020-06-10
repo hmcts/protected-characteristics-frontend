eec496f15462307ad5b32ebefb5c2ba9e7864773
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-06-10 13:00:36 +0100

    Add actualStatus back for backwards compatibility

--- app/healthcheck.js
+++ app/healthcheck.js
@@ -13,15 +13,18 @@ const gitProperties = require('git.properties');
 const gitCommitId = gitProperties.git.commit.id;
 const gitRevision = process.env.GIT_REVISION;
 
+const statusComment = '\'actualStatus\' is the same as \'status\'. It is there for backwards compatibility. Please disregard.';
 const checks = {
     'pcq-backend': healthcheck.web(`${config.services.pcqBackend.url}/health`, {
         callback: (err, res) => {
-            const status = err ? 'DOWN' : res.body.status;
+            const status = err ? 'DOWN' : res.body.status || 'DOWN';
             if (status === 'DOWN') {
                 logger.warn('pcq-backend is DOWN');
                 logger.warn(err);
             }
-            return status === 'UP' ? healthcheck.up() : healthcheck.down();
+            // DEPRECATED: To be removed after all services stop depending on 'actualStatus'
+            const options = {actualStatus: status, comment: statusComment};
+            return status === 'UP' ? healthcheck.up(options) : healthcheck.down(options);
         },
         timeout: 10000,
         deadline: 20000
