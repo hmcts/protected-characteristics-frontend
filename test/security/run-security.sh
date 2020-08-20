ec733d9651efaf125544a19187ebbd7c0c7d4c0a
riyan <venkatd648@gmail.com>
2020-08-20 16:28:34 +0100

    updated security.sh file

--- test/security/run-security.sh
+++ test/security/run-security.sh
@@ -1,10 +1,11 @@
-set -e
-echo "Running security testing on: $1"
-zap-cli --zap-url http://127.0.0.1 -p 8090 status -t 120
-zap-cli --zap-url http://127.0.0.1 -p 8090 open-url "$1"
-zap-cli --zap-url http://127.0.0.1 -p 8090 spider "$1"
-zap-cli --zap-url http://127.0.0.1 -p 8090 active-scan  --scanners all --recursive "$1"
-zap-cli --zap-url http://127.0.0.1 -p 8090 report -o 'security-reports/active-scan.html' -f html
-zap-cli --zap-url http://127.0.0.1 -p 8090 ajax-spider "$1"
-zap-cli --zap-url http://127.0.0.1 -p 8090 report -o 'security-reports/ajax-spider.html' -f html
-zap-cli --zap-url http://127.0.0.1 -p 8090 alerts -l High
+#!/usr/bin/env bash
+echo ${TEST_URL}
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 8090 -l FAIL
+cat zap.out
+zap-cli --zap-url http://0.0.0.0 -p 8090 report -o /zap/api-report.html -f html
+echo "listings of zap folder"
+ls -la /zap
+cp /zap/api-report.html functional-output/
+curl --fail http://0.0.0.0:8090/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+cp *.* functional-output/
+zap-cli --zap-url http://0.0.0.0 -p 8090 alerts -l High --exit-code False
