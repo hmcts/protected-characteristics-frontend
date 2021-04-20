f931577f184e514e93b10d1f016b530cc5176fe1
Dave Jones <38425925+davejones74@users.noreply.github.com>
2021-04-20 14:23:19 +0100

    PCQ-839: Updated sslOptions to use same TSL ver

--- app.js
+++ app.js
@@ -252,7 +252,8 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
     if (['development', 'testing'].includes(config.nodeEnvironment)) {
         const sslDirectory = path.join(__dirname, 'app', 'resources', 'localhost-ssl');
         const sslOptions = {
-            secureProtocol: 'TLSv1_2_method',
+            minVersion: 'TLSv1.2',
+            maxVersion: 'TLSv1.2',
             key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
             cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
         };
